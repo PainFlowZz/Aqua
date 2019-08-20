2ca3a9fa8faf08a4ce5ae31423bcb3068f729cc8
Pain <painflowzz@gmail.com>
2019-08-20 17:49:36 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -27,6 +27,7 @@ exports.run = (client, message, args) => { // eslint-disable-line no-unused-vars
             .addField("Users", client.users.size.toLocaleString(), true)
             .addField("CPU usage", percent.toFixed(2)+ "%", true)
             .addField("Developers", info.developers, true)
+            .addBlankField()
             .setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL)
             .setTimestamp()
         message.channel.send(embedStats)
