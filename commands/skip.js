69fdda0c9e608ee3fe4a2e0d2dbf0b187ed225a6
Pain <painflowzz@gmail.com>
2019-08-20 18:18:58 +0200

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,13 +1,10 @@
-const Discord = require("discord.js");
+exports.run = async => {
 
-exports.run = async (msg, args, guild) => {
-    const queue = new Map();
-    const serverQueue = queue.get(msg.guild.id);
-    
-    if (!msg.member.voiceChannel) return msg.channel.send('Please join a Voice Channel to use music commands!');
-		
-    if (!serverQueue) return msg.channel.send('The queue is empty!');
-    
-    serverQueue.connection.dispatcher.end('Successfully skipped!');
-    return undefined; 
+}
+
+exports.config = {
+    name: "skip",
+    usage: "!skip",
+    description: "Skips the current song!",
+    accessableby: "Everyone"
 }
