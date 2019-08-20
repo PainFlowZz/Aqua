75fe36583d78183ac7b7f1559653ab0d7ede7f2c
Pain <painflowzz@gmail.com>
2019-08-20 16:33:59 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -18,7 +18,7 @@ exports.run = (client, message, args) => { // eslint-disable-line no-unused-vars
         const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
         const embedStats = new Discord.RichEmbed()
             .setColor(colour)
-            .addField("Aqua | アクア", client.user.avatarURL)
+            .addTitle("Aqua | アクア", client.user.avatarURL)
             .addField("Version", info.version, true)
             .addField("Library", info.library, true)
             .addField("Website", info.website, true)
