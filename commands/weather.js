b469b697b5d890891ea3149aff7252b82675cbde
Pain <painflowzz@gmail.com>
2019-08-22 01:38:31 +0200

    1

--- commands/weather.js
+++ commands/weather.js
@@ -3,36 +3,27 @@ const weather = require('weather-js');
 const { colour } = require ("../colours.json");
 const { rcolour } = require ("../colours.json");
 
-const e1 = new Discord.RichEmbed()
-.setColor(rcolour)
-.setDescription('❯ Please enter a location! `❌`')
-
-const e2 = new Discord.RichEmbed()
-.setColor(rcolour)
-.setDescription('❯ Please enter a valid location! `❌`')
-
 exports.run = async (client, message, args) => {
   
-  let eemoji = message.guild.emojis.find(x => x.name === "9330_tickred");
-  if(!args[0]) return message.channel.send(e1);
+  if(!args[0]) return message.channel.send("Please specify a location!");
   
   weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
     if (err) message.channel.send(err);
-    if (result === undefined || result.length === 0) return message.channel.send("Invalid Location " + eemoji);
+    if (result === undefined || result.length === 0) return message.channel.send("Pleasy specify a valid location!");
 
     var current = result[0].current;
     var location = result[0].location;
     const embed = new Discord.RichEmbed()
     .setDescription(`**${current.skytext}**`)
-    .setAuthor(`❯  ${current.observationpoint}`)
+    .setTitle(`${current.observationpoint}`, client.user.avatarURL)
     .setThumbnail(current.imageUrl)
     .setColor(colour)
-    .addField('❯ Timezone',`UTC${location.timezone}`, true)
-    .addField('❯ Degree Type',location.degreetype, true)
-    .addField('❯ Temperature',`${current.temperature} Degrees`, true)
-    .addField('❯ Feels Like', `${current.feelslike} Degrees`, true)
-    .addField('❯ Winds',current.winddisplay, true)
-    .addField('❯ Humidity', `${current.humidity}%`, true)
+    .addField('Timezone',`UTC${location.timezone}`, true)
+    .addField('Degree Type',location.degreetype, true)
+    .addField('Temperature',`${current.temperature} Degrees`, true)
+    .addField('Feels Like', `${current.feelslike} Degrees`, true)
+    .addField('Winds',current.winddisplay, true)
+    .addField('Humidity', `${current.humidity}%`, true)
     message.channel.send({embed});
   })
 }
