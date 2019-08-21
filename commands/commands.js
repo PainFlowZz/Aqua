7df5814bdb787c23d1930eec72c528cb7882e824
Pain <painflowzz@gmail.com>
2019-08-21 22:37:29 +0200

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,19 +3,17 @@ const { colour } = require ("../colours.json");
 
 module.exports.run = async (bot, message, args, settings) => {
     
-    let emoji = message.guild.emojis.find(x => x.name === "aqua_dance");
+    let emoji = message.guild.emojis.find(x => x.name === "minecraft_chest");
 
     const prefix = settings.prefix
 
-        const HelpEmbed = new Discord.RichEmbed()
-        .setColor(colour)
-        .setTitle(emoji + "**Commands!**")
-        .setDescription(`If you need help with a command, run ${prefix}help <command>`)
-        .addField('➜ Commands', "If you need help with a command, please run " + prefix +"help `<command>`. If you need a list of all commands, please run " + prefix + "commands.")
-        .addField('➜ Issues', "If you run into any issues such as bugs or errors, please join the support server [here](https://discord.gg/kz4tzW).")
-        .addField('➜ Suggestions', "If you have any suggestions, direct message my senpai pain.exe#0001. ")
+    const HelpEmbed = new Discord.RichEmbed()
+    .setColor(colour)
+    .setTitle(emoji + "**Commands!**")
+    .setDescription(`If you need help with a command, run ${prefix}help <command>`)
 
-        message.channel.send(HelpEmbed)
+    message.channel.send(emoji+ " Take a look at your DMs!")
+    message.author.send(HelpEmbed)
     
 }
 
