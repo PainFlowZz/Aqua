a16fc8efda84538b85c792395665dae432623e63
pain <prxl.px@gmail.com>
2019-08-25 14:12:40 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,13 +4,8 @@ const ytdlDiscord = require('ytdl-core-discord');
  
 exports.run = async (message, args) => {
  
-    let voiceChannel = message.member.voiceChannel;
-
-    if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has('CONNECT')) return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
-    if (!permissions.has('SPEAK')) return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
-
+    if (!message.member.voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+ 
     const serverQueue = message.client.queue.get(message.guild.id);
     const songInfo = await ytdl.getInfo(args[0]);
     const song = {
