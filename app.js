3da67d6f23d60f622c6f6b63655ecd0f214ac210
pain <prxl.px@gmail.com>
2019-08-25 13:40:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const client = new MusicClient({ token: process.env.DISCORD_TOKEN });
 const { colour } = require ("./colours.json");
 const { rcolour } = require ("./colours.json");
 const YouTube = require('simple-youtube-api');
+const { readdirSync } = require('fs');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const ytdl = require('ytdl-core');
