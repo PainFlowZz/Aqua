c7fa9335014811072296b2feae0658befc0c871e
pain <prxl.px@gmail.com>
2019-08-25 13:42:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const fs = require('fs');
 const Discord = require ("discord.js")
 const MusicClient = require('./struct/Client');
 const client = new MusicClient({ token: process.env.DISCORD_TOKEN });
-const { colour } = require ("./colours.json");
-const { rcolour } = require ("./colours.json");
-const YouTube = require('simple-youtube-api');
 const { readdirSync } = require('fs');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const ytdl = require('ytdl-core');
-const Util = require("discord.js");
+const { join } = require('path');
 
 require('dotenv-flow').config();
 require('./utils/functions')(client);
